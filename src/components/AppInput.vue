abfb2f27f4d8cb1923f777ccba7f31ebabadf2dd
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-02-28 17:39:26 +0100

    Refactor AppInput

--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -1,3 +1,17 @@
+<template>
+  <div class="base-input">
+    <label v-show="label" :for="id">{{ label }}</label>
+    <input
+      :id="id"
+      ref="input"
+      class="base-input__input"
+      :type="inputType"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="updateHandler"
+    />
+  </div>
+</template>
 <script lang="ts">
 import { defineComponent } from "vue";
 export default defineComponent({
@@ -19,6 +33,10 @@ export default defineComponent({
       type: String,
       require: true,
     },
+    inputType: {
+      type: String,
+      default: "text",
+    },
   },
   setup(_, { emit }) {
     const updateHandler = (event: any) => {
@@ -30,21 +48,6 @@ export default defineComponent({
   },
 });
 </script>
-<template>
-  <div class="base-input">
-    <label v-show="label" :for="id">{{ label }}</label>
-    <input
-      :id="id"
-      ref="input"
-      class="base-input__input"
-      type="text"
-      :placeholder="placeholder"
-      :value="modelValue"
-      @input="updateHandler"
-    />
-  </div>
-</template>
-
 <style lang="postcss" scoped>
 .base-input {
   @apply w-full flex flex-col items-start py-2 px-4;
